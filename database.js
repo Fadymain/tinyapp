8ae4e75a774d8bcf074cdd571ea4c06b1da0357f
Adetola Fadairo <adetolafadairo@gmail.com>
2022-02-26 21:19:16 +0000

    created database file so it could be required without interfering with helpers file

---
+++ database.js
@@ -0,0 +1,22 @@
+const bcrypt = require("bcryptjs");
+
+const urlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userId: "userRandomID"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userId: "userRandomID22"
+  }
+}
+
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: bcrypt.hashSync("purple-monkey-dinosaur", 10)
+  }
+}
+
+module.exports = {urlDatabase, users};
